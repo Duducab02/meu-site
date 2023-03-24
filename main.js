1235202a2ef0be4bbc53486db677ca0b6c07b872
Duducab02 <128853016+Duducab02@users.noreply.github.com>
2023-03-24 17:35:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarcorleave(){
+    document.getElementById("titulo").style.color="rgb(5, 233, 81)"}
+    function mudarcorover(){
+    document.getElementById("titulo").style.color="rgb(5, 149, 233)"
+    }
+    function proximapagina(){
+    window.location="portfolio.html"
+    }
